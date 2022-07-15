ff452c29bca6f8efc7fe6552a0183cec5b21da61
Jocelyn Badgley <jocelyn.badgley@zenbusiness.com>
2022-07-15 10:41:01 -0700

    feat(b): Publishing package B

---
+++ packages/b/index.js
@@ -0,0 +1,31 @@
+const {
+  isObject,
+} = require('@twipped/monorepo-test-a');
+
+/**
+ * Combines multiple objects into one, recursively.
+ *
+ * @param   {...Object} sources Objects to copy.
+ *
+ * @returns {Object}
+ * @category Data
+ */
+module.exports = exports = function merge (...sources) {
+  const result = {};
+  for (const source of sources) {
+    if (!source) continue;
+
+    for (const [ key, value ] of Object.entries(source)) {
+      if (isObject(value)) {
+        if (isObject(result[key])) {
+          result[key] = merge(result[key], value);
+        } else {
+          result[key] = merge(value);
+        }
+      } else {
+        result[key] = value;
+      }
+    }
+  }
+  return result;
+};
